cce08aa4c5e670c3e6f7e334ed05126041ae26e7
Mikhail Libchenko <libchenkomikhail@gmail.com>
2025-03-11 17:40:45 +0300

    Initial commit

---
+++ Source/Presentation/BaCS.MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BaCS.MAUI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Source/Presentation/BaCS.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BaCS.MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
