eedece49af1e2990b5fa957b9ef9c9ee839d8173
Mikhail Libchenko <libchenkomikhail@gmail.com>
2025-04-05 00:59:10 +0300

    BACS-18: chore: TracingPipelineBehavior added

--- Source/Application/BaCS.Application.Handlers/RegistrationExtensions.cs
+++ Source/Application/BaCS.Application.Handlers/RegistrationExtensions.cs
@@ -2,6 +2,7 @@ namespace BaCS.Application.Handlers;
 
 using System.Reflection;
 using Authorization;
+using Behaviours;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,7 +11,9 @@ public static class RegistrationExtensions
     public static IServiceCollection AddApplicationHandlers(this IServiceCollection services)
     {
         services.AddMediatR(
-            cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
+            cfg => cfg
+                .RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
+                .AddOpenBehavior(typeof(TracingPipelineBehavior<,>))
         );
 
         services.AddMemoryCache();
