f5431dfa24b2df4e54d9d9718d5873b3f29b10a3
Mikhail Libchenko <libchenkomikhail@gmail.com>
2025-04-12 21:00:50 +0300

    BACS-22: chore: minor analyzer updates

--- Source/Application/BaCS.Application.Integrations/Email/Constants/EmailTemplateConstants.cs
+++ Source/Application/BaCS.Application.Integrations/Email/Constants/EmailTemplateConstants.cs
@@ -2,9 +2,9 @@ namespace BaCS.Application.Integrations.Email.Constants;
 
 public static class EmailTemplateConstants
 {
-    private const string BasePath = "BaCS.Application.Integrations.Email.Templates";
-
     public const string ReservationCreated = $"{BasePath}.ReservationCreated.html";
     public const string ReservationUpdated = $"{BasePath}.ReservationUpdated.html";
     public const string ReservationCancelled = $"{BasePath}.ReservationCancelled.html";
+
+    private const string BasePath = "BaCS.Application.Integrations.Email.Templates";
 }
