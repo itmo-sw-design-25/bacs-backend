658e8176ecc01231b6b89ae49396b03d310fe517
Mikhail Libchenko <libchenkomikhail@gmail.com>
2025-04-13 00:30:49 +0300

    BACS-23: feat: user email made editable

---
+++ Source/Persistence/BaCS.Persistence.PostgreSQL/Migrations/20250412212608_User__Email_Made_Not_Unique.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaCS.Persistence.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class User__Email_Made_Not_Unique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_users_email",
+                table: "users");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                table: "users",
+                column: "email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_users_email",
+                table: "users");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+    }
+}
